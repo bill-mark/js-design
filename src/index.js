e0ab512ca5126a7266b8b0a11887d0ecde2442ce
bill.mark <weiguicheng@foxmail.com>
2018-08-02 10:01:16 +0800

    up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,126 @@
-alert(100);
+ class jQuery  {
+	  constructor(seletor){
+       let slice = Array.prototype.slice
+       let dom = slice.call(document.querySelectorAll(seletor))
+       let len = dom ? dom.length:0
+       for(let i=0;i<len;i++){
+       	 this[i] = dom[i]
+       }
+       this.length = len
+       this.seletor = seletor || ''
+	  }
+	  append(node){
+
+	  }
+	  addClass(name){
+
+	  }
+	  html(data){
+
+	  }
+ }
+
+ window.$ = function (selector) {  //实例化JQ
+ 	//工厂模式
+ 	return new jQuery(selector)
+ }
+
+ var $p = $('p')
+ console.log($p)
+ console.log($p.addClass)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class People{
+// 	constructor(name){
+// 		this.name = name
+// 	}
+// }
+// class A extends People{
+// 	constructor(name){
+// 		super(name)
+// 	}
+// 	saySomething(){
+// 		alert('i an a')
+// 	}
+// }
+// class B extends People{
+// 	constructor(name){
+// 		super(name)
+// 	}
+// 	saySomething(){
+// 		alert('i am b')
+// 	}
+// }
+// let a = new A('a')
+// a.saySomething()
+// let b = new B('b')
+// b.saySomething()
+
+
+
+// class People{
+// 	constructor(name,age){
+// 		this.name = name
+// 		this.age = age
+// 	}
+// 	eat(){
+// 		alert(this.name +' eat something')
+// 	}
+// 	speak(){
+// 		alert('my name is ' +this.name +' , age ' + this.age)
+// 	}
+// }
+ 
+// class Student extends People{
+// 	constructor(name,age,number){
+// 		super(name,age)
+// 		this.number = number
+// 	}
+//     study(){
+//         alert(this.name + ' study')	
+//     }
+// }
+
+// let xiaoming = new Student('xiaoming',21,'A1')
+// xiaoming.study()
+// alert(xiaoming.number)
+
+
+// let zhang = new People('zhang',20)
+// console.log(zhang)
+// zhang.eat()
+// zhang.speak()
+
+// let wang = new People('wang',21)
+// wang.eat()
+// wang.speak()
+
+
+
+// class People {
+// 	constructor(name){
+// 		this.name = name
+// 	}
+// 	getName(){
+// 		return this.name
+// 	}
+// }
+
+// let p = new People('牛sX老师')
+// alert(p.getName())
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,29 @@
+ const path  = require('path')
+ const HtmlWebpackPlugin = require('html-webpack-plugin')
+
  module.exports = {
  	entry:'./src/index.js',
  	output:{
  		path:__dirname,
- 		filename:'./release/bundle.js'
+ 		filename:'./release/bundle.js'  //release 会自动创建
+ 	},
+
+ 	module:{
+       rules:[{
+       	  test:/\.js?$/,
+       	  exclude:/(node_modules)/,
+          loader:'babel-loader'
+       }]
+ 	},
+
+ 	plugins:[
+       new HtmlWebpackPlugin({
+       	   template:'./index.html'  //bundle.js会自动注入
+       })
+ 	],
+ 	devServer:{
+ 		contentBase:path.join(__dirname,'./release'),  //根目录
+ 		open:true,//自动打开浏览器窗口
+ 		port:9000,
  	}
  }
